e35f0108528b00b1c0d214692ee2c5690f72e2ae
Bianca Cena <bibcena21@gmail.com>
2024-06-01 01:25:45 +0800

    Update [longest_increasing_subsequence] : New files and changes for problem_set_3.
    - Refactored code in longest_increasing_subsequence.js as the code was messy and inaccurate.
    - Added new file, same answer but code is written in C#.
    - Added an executionable file for C# program.

--- problem_set_3/longest_increasing_subsequence.js
+++ problem_set_3/longest_increasing_subsequence.js
@@ -1,87 +1,78 @@
-/*
-Sample Given Input:
-Input: [10, 9, 2, 5, 3, 7, 101, 18]
-Corrent Answer: [2, 5, 7, 18]
-Output: 4
+function lengthOfLIS(numbers) {
+	let lis = 0;
 
- */
+	if (numbers.length === 0) return lis;
 
-function lengthOfLIS(nums) {
-	let finalSequences = nums.map((item) => [item]);
-	let repeat = Math.floor(nums.length / 2) - 1;
-	let count = 0;
+	// Create an array for each number
+	let nestedArray = [];
+	// let nestedArray = numbers.map((item) => [item]);
+	// let lis = 0;
 
-	while (count < repeat) {
-		finalSequences.forEach((element) => {
-			let sequences = compare(nums, element);
-			sequences.forEach((sequence) => {
-				if (!arrayExistsInNestedArray(sequence, finalSequences))
-					finalSequences.push(...sequences);
-			});
-		});
-		count++;
-	}
-}
+	// Using c as the index, run a loop through the nested array
+	for (let c = 0; c < numbers.length; c++) {
+		//Initialize the subArray with the current number
+		let subArray = [numbers[c]];
 
-function compare(original, array) {
-	// WIll store all the sequences that will be found.
-	let sequences = [];
+		// Get the element of current child array
+		// lastElement refers to the last element pushed to the child array
+		let lastElement = subArray[subArray.length - 1];
 
-	// Get last index of array
-	let lastIndex = array.length - 1;
-	// Access last element of array
-	let lastElement = array[lastIndex];
-	// Determine the start of the loop
-	let start = original.indexOf(lastElement) + 1;
-
-	// console.log("CURRENT ARRAY IS " + JSON.stringify(array) + "\n");
-	for (i = start; i < original.length; i++) {
-		// Check if index of lastElement and original[i] are not the same
-		// Compare the two values
-		if (lastIndex !== i && lastElement < original[i]) {
-			// Append the array and the current element to new array
-			let newArray = [...array, original[i]];
-			// Add new array to sequences
-			if (newArray.length > 0) sequences.push(newArray);
+		// Using n as the index, run a loop through the numbers array
+		// Start n with the value of c
+		for (let n = c; n < numbers.length; n++) {
+			if (lastElement < numbers[n]) {
+				// Add element to child array
+				subArray.push(numbers[n]);
+				// Update the current element of the child array
+				lastElement = numbers[n];
+			}
 		}
+
+		// Insert list to nested array
+		nestedArray.push(subArray);
+
+		// Update the lis
+		if (subArray.length > lis)
+			// Update lis
+			lis = Math.max(lis, subArray.length);
 	}
 
-	return sequences;
-}
-
-function arrayExistsInNestedArray(arr, nestedArr) {
-	return nestedArr.some(
-		(subArr) => JSON.stringify(subArr) === JSON.stringify(arr)
-	);
+	return lis;
 }
 
 // Main function
 function main() {
-	const readline = require("node:readline");
+	// let numbers = [10, 9, 2, 5, 3, 7, 101, 18];
+	// console.log("Input: " + numbers);
+	// const result = lengthOfLIS(numbers);
+	// console.log("Output: " + result);
 
+	// - - - - - - - FOR MANUAL INPUT - - - - - - - //
+	const readline = require("node:readline");
 	const rl = readline.createInterface({
 		input: process.stdin,
 		output: process.stdout,
 	});
+
+	let numbers = [];
 
 	// Keep reading values until the condition is false
 	console.log("Enter a list of numbers, Ctrl + C to stop.");
 	rl.setPrompt("Input: ");
 	rl.prompt();
 	rl.on("line", function (line) {
-		if (line && line.trim()) {
-			const num = Number(line.trim());
-			if (!isNaN(num)) {
-				nums.push(num);
-			}
+		const number = Number(line.trim());
+
+		if (Number(number)) {
+			numbers.push(number);
+		} else {
+			console.log("Invalid");
 		}
+
 		rl.prompt();
 	}).on("close", function () {
-		if (nums && nums.length !== 0) {
-			// Check input
-			console.log(nums);
-			const result = lengthOfLIS(nums);
-		}
+		const result = lengthOfLIS(numbers);
+		console.log("Output: " + result);
 		process.exit(0);
 	});
 }
